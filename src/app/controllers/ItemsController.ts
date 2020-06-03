9f32ed20f46506053b2aab548b703d75e6e04b68
Matheus Rocha <matheus_rocha98@outlook.com>
2020-06-03 04:49:12 -0300

    fix: getting image_url based on NODE_ENV

--- src/app/controllers/ItemsController.ts
+++ src/app/controllers/ItemsController.ts
@@ -6,13 +6,15 @@ import knex from '../../database/connection';
 class ItemsController {
   async index(req: Request, res: Response) {
     const items: Item[] | null = await knex.select(['id', 'title', 'image']).from<Item>('items');
-    const { HOST, PORT } = process.env;
+    const { HOST, PORT, NODE_ENV, URL } = process.env;
 
+    const getParsedImageURL = (image: string) =>
+      NODE_ENV === 'development' ? `http://${HOST}:${PORT}/${image}` : `${URL}/${image}`;
     const serializedItems = items?.map(({ id, title, image }) => {
       return {
         id,
         title,
-        image_url: `http://${HOST}:${PORT}/${image}`,
+        image_url: getParsedImageURL(image),
       };
     });
     return res.status(OK).json(serializedItems);
